50dd9db1056928cb728d75be0ca337de0b6f4203
Pyth0nHater <116113690+Pyth0nHater@users.noreply.github.com>
2023-03-10 15:53:06 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,100 +1,155 @@
-#include <iostream> 
+#include <iostream>
 #include <fstream>
-
 using namespace std;
 
-int main() {
+void symbolInStr(char *str, char *str2, char *symbol)
+{
 
-    int beginWord, endWord = 0;
-    char str[256];
-    char str2[256];
-
-    char symbol[1];
-
-
-
+    int LenStr = sizeof(str);
+    int LenStr2 = sizeof(str2);
     int symbolInStr = 0;
     int symbolInStr2 = 0;
-    int i = 0;
-    cout << str << endl;
 
-const char* FileName = "File.txt";
+    for (int i = 0; i <= LenStr; i++)
+    {
+        if (str[i] == symbol[0])
+        {
+            symbolInStr++;
+        }
+    }
 
- ifstream fin(FileName);
+    for (int i = 0; i <= LenStr2; i++)
+    {
+        if (str2[i] == symbol[i])
+        {
+            symbolInStr2++;
+        }
+    };
 
-  if (!fin) {
-    cout << "Файл не открыт!" << endl;
-  }
-
-  fin.getline(symbol,1);
-  fin.getline(str,256);
-  fin.getline(str2,256);
-
-  cout<<symbol;
-  cout<<str;
-  cout<<str2;
-
+    // сравниваем где больше
+    if (symbolInStr > symbolInStr2)
+    {
+        cout << str << endl;
+    }
+    else
+    {
+        cout << str2 << endl;
+    };
+};
 
 
+void DublicateOfWord(char *str, char *str2){
+    int i,j,l,k,m,o = 0;
+    char povtorSlova[256];
+    int beginWord, endWord, beginWord2, endWord2 = 0;
+    int lenStr = 0;
+    int lenStr2 = 0;
+    bool fl = true;
 
-  //проверки
-  if ((fin.eof()) && (fin.fail())) { 
-    cout << "Файл пуст!" << endl;
-    fin.close();
-  }
 
-  if (fin.fail()) {
-    cout << "Сбой чтения!" << endl;
-    cout << "Проверьте корректность данных в файле!" << endl;
-  }
-
-    while (str[i] != '\0') {
-        while ((str[i] != '\0') && ( str[i] == ' ') )
+//инициализация переменных
+    //идем до конца 1й строки
+    while (str[i] != '\0') 
+    {
+        while ((str[i] != '\0') && (str[i] == ' '))
         {
             i++;
         }
-        beginWord = i;
+        beginWord = i;//первый элемент
+        //
         while (str[i] != '\0' && str[i] != ' ') {
             i++;
         }
-        endWord = i;
-        for (int j = beginWord; j <= endWord; j++) {
-            for(int k = beginWord; k <= endWord; k++){
-        
-            if (str[j] == symbol[0]){
-                    symbolInStr++;
-        }  
-    }
-    }
-    }
-    
-//2
-    i = 0;
-    cout << str2 << endl;
-     while (str2[i] != '\0') {
-        while ((str2[i] != '\0') && ( str2[i] == ' ') )
-        {
-            i++;
-        }
-        beginWord = i;
-        while (str2[i] != '\0' && str2[i] != ' ') {
-            i++;
-        }
-        endWord = i;
-        for (int j = beginWord; j <= endWord; j++) {
-            if (str2[j] == symbol[0]){
-                    symbolInStr2++;
-            }
-        }  
-    
-    }
-    if (symbolInStr > symbolInStr2){
-        cout<<str<<endl;
-    }
-    else{
-        cout<<str2<<endl;
+        endWord = i - 1;// последний элемент
+        lenStr = endWord - beginWord+1;//длина слова 1 строки
 
+        //2 строка
+        while (str2[j] != '\0') {
+            while ((str2[j] != '\0') && (str2[j] == ' ')) {
+                j++;
+            }
+            beginWord2 = j;//1 элемент
+            while (str2[j] != '\0' && str2[j] != ' ') {
+                j++;
+            }
+            endWord2 = j - 1;//последний элемент
+            lenStr2 = endWord2 - beginWord2+1;//длина слова 2 строки
+
+            //сравниваем длины
+            if (lenStr == lenStr2) {
+                m = beginWord2;//1 элемент слова со 2 строки
+                fl = true;// поднимает флаг
+                for (int k = beginWord; k <= endWord; k++) { //идем по длине
+                        if (str[k] == str2[m]) {             //если элементы совпали
+                            povtorSlova[o] =  str[k];        //записываем 1 элемент в повтор слова
+                            o++;                             //шаг вперед в повторе слова
+                            m++;                             //шаг вепед в слове из 2 строки
+                        }
+                        else{
+                            fl = false;                      // опускаем флаг
+                    }
+                }
+                m = 0;                                       //обнуляем индексы
+                o = 0;  
+                if(fl){                                      //если флаг поднят
+                    cout<<povtorSlova<<endl;                 //выводим
+                }
+            }
+        }
     }
+
+}
+
+int main()
+{
+    // инициализация переменных
+
+    const char *FNAME = "D:\\VS\\AllLabs\\test1.txt";
+    // const char *FNAME = "D:\\VS\\AllLabs\\test2.txt";
+    // const char *FNAME = "D:\\VS\\AllLabs\\test3.txt";
+    // const char *FNAME = "D:\\VS\\AllLabs\\test4.txt";
+    // const char *FNAME = "D:\\VS\\AllLabs\\test5.txt";
+    // const char *FNAME = "D:\\VS\\AllLabs\\test6.txt";
+    // const char *FNAME = "D:\\VS\\AllLabs\\test7.txt";
+    // const char *FNAME = "D:\\VS\\AllLabs\\test8.txt";
+
+    char str[256];
+    char str2[256];
+    char symbol[256];
+
+
+    // начало чтения
+    ifstream fin(FNAME);
+
+    if (!fin)
+    {
+        cout << "file " << FNAME << " not found" << endl;
+        return 1; // выход по ошибке
+    }
+
+
+    // читаем из файла
+    fin.getline(symbol, 10); //1 строка
+    fin.getline(str, 255); //2 строка
+    fin.getline(str2, 255); //3 строка
+
+    // проверки состояния файла
+    if ((fin.eof()) && (fin.fail())) // файл пуст
+    {
+        cout << "file " << FNAME << " empty" << endl;
+        fin.close(); // закрыть файл
+        return 2;    // выход по ошибке
+    }
+
+    //Вывод строк из файла
+    cout << "Symbol = <" << symbol << ">" << endl;
+    cout << "Str 1 = <" << str << ">" << endl;
+    cout << "Str 2 = <" << str2 << ">" << endl;
+    // конец четния
+
+    //Применение функций
+    symbolInStr(str, str2, symbol);//Первая функция
+    DublicateOfWord(str, str2);//Вторая функция
 
     return 0;
 }
